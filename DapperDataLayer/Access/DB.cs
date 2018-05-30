48d0b741ad24ecd1f813029dbe8db01913812743
Dağhan Kaban <dkaban@hotmail.com>
2018-05-30 18:14:54 +0300

    flush update

--- DapperDataLayer/Access/DB.cs
+++ DapperDataLayer/Access/DB.cs
@@ -10,198 +10,248 @@ using System.Threading.Tasks;
 
 namespace DDLayer.Access
 {
-   
-    public class DB:IDisposable
-    {
-        private List<Dparam> parametreler;
-        private List<Dparam> outputparametreler;
-        
-        public DB()
-        {
-            parametreler = new List<Dparam>();
-            outputparametreler = new List<Dparam>();
-        }
 
-        /// <summary>connection string ve query alır. Geri dönüş tipi olmayan querylerde generic tipi olarak dynamic kullanılabilir.<para />
-        /// Generic metod oldugu için tiplerin baştan belirtilmesi gerekir.<para />
-        /// TEKRAR TEKRAR AYNI DB NESNESI KULLANILACAKSA ARALARINDA FLUSHPARAMETER() METODU KULLANILMALI <para />
-        /// </summary>
-        public List<T> SelectQuery<T>(string query)
-        {
-            try {using (IDbConnection _db = new SqlConnection(Helper.connectionstring())) { return _db.Query<T>(query).ToList(); } }         
-            catch (Exception e) 
-            {
-                e = e;
-                throw;
-            }        
-        }
+	public class DB : IDisposable
+	{
+		private List<Dparam> parametreler;
+		private List<Dparam> outputparametreler;
 
-        /// <summary>**!!!Sadece Düz parametre alan procedureler için. Output parametre almaz.!!!***<para />
-        /// Generic metod oldugu için tiplerin baştan belirtilmesi gerekir.       <para />
-        /// TEKRAR TEKRAR AYNI DB NESNESI KULLANILACAKSA ARALARINDA FLUSHPARAMETER() METODU KULLANILMALI <para />
-        /// </summary>
-        public List<T> SelectSP<T>(string procadi)
-        {
-            try
-            {
-                using (IDbConnection _db = new SqlConnection(Helper.connectionstring()))
-                {
-                    var p = new DynamicParameters();
-                    foreach (var item in parametreler) { p.Add(item.name, item.value); }
-                    var donenliste = _db.Query<T>(procadi, p, commandType: System.Data.CommandType.StoredProcedure);
-                    //var yorumlar = _db.Query<AracModel>("ap_AracModel", p, commandType: System.Data.CommandType.StoredProcedure);
-                    List<T> sonliste = new List<T>();
-                    foreach (var item in donenliste) { sonliste.Add(item); }
-                    return sonliste;
-                }
-            }
-            catch (Exception e)
-            {
-                e = e;
-                throw;
-            }
-        }
-        /// <summary>***!!!OUTPUTLU PROCLAR ICIN.!!!***<para />
-        /// Dönüş değeri generictir. outputs nesnesi geriye name-value ikilisi olarak output degerleri doner.<para />
-        /// ÇOK ÖNEMLİ NOT: OUTPUT OLMAYAN PARAMETRELERDE KESİNLİKLE DBTYPE GİRİLMEMELİ. DBTYPE OLANLAR SADECE OUTPUT PARAMETRELERİ<para />
-        /// TEKRAR TEKRAR AYNI DB NESNESI KULLANILACAKSA ARALARINDA FLUSHPARAMETRE() METODU KULLANILMALI <para />
-        /// Generic metod oldugu için tiplerin baştan belirtilmesi gerekir. <para />
-        /// outputs=> gerıye donen output parametreleri<para />
-        /// Ek-Not: Float==>Single ama Float değerleri doublela karsılamak gerekiyor nedense.<para />
-        /// </summary>
-        public List<T> SelectSP<T>(string procadi,out List<Dparam> outputs)
-        {
-            try
-            {
-                var p = new DynamicParameters(); List<T> donenliste = new List<T>();
-                using (IDbConnection _db = new SqlConnection(Helper.connectionstring()))
-                {
-                                      
-                    foreach (var item in parametreler) { p.Add(item.name, item.value); }
-                    foreach (var item in outputparametreler) {p.Add(item.name, dbType: item.dbtype, direction: ParameterDirection.Output); }
-                    var returner = _db.Query<T>(procadi, p, commandType: System.Data.CommandType.StoredProcedure);                     
-                    foreach (var item in returner) { donenliste.Add(item); }                  
-                    outputs = new List<Dparam>();
-                    foreach (var item in outputparametreler) { outputs.Add(Dparam.typecaster(p, item.dbtype, item.name)); }
-                         
-                }
-                //double a = p.Get<double>("@BORC");
-                //double b = p.Get<double>("@ALACAK");
-                return donenliste;
-            }
-            catch (Exception e) 
-            {
-                e = e;
-                throw;
-            }
+		public DB()
+		{
+			parametreler = new List<Dparam>();
+			outputparametreler = new List<Dparam>();
+		}
 
-        }
+		/// <summary>connection string ve query alır. Geri dönüş tipi olmayan querylerde generic tipi olarak dynamic kullanılabilir.<para />
+		/// Generic metod oldugu için tiplerin baştan belirtilmesi gerekir.<para />
+		/// TEKRAR TEKRAR AYNI DB NESNESI KULLANILACAKSA ARALARINDA FLUSHPARAMETER() METODU KULLANILMALI <para />
+		/// </summary>
+		public List<T> SelectQuery<T>(string query,bool flush)
+		{
+			try
+			{
+				using (IDbConnection _db = new SqlConnection(Helper.connectionstring()))
+					return _db.Query<T>(query).ToList();
+			}
+			catch (Exception e)
+			{
+				throw;
+			}
+			finally {
+				if (flush)
+					FlushParameter();
+			}
+		}
 
+		/// <summary>**!!!Sadece Düz parametre alan procedureler için. Output parametre almaz.!!!***<para />
+		/// Generic metod oldugu için tiplerin baştan belirtilmesi gerekir.       <para />
+		/// TEKRAR TEKRAR AYNI DB NESNESI KULLANILACAKSA ARALARINDA FLUSHPARAMETER() METODU KULLANILMALI <para />
+		/// </summary>
+		public List<T> SelectSP<T>(string procadi,bool flush)
+		{
+			try
+			{
+				using (IDbConnection _db = new SqlConnection(Helper.connectionstring()))
+				{
+					var p = new DynamicParameters();
+					foreach (var item in parametreler)
+						p.Add(item.name, item.value);
 
-        /// <summary>***Geri dönüş yapmayan procedureler için***<para />
-        /// ÇOK ÖNEMLİ NOT: OUTPUT OLMAYAN PARAMETRELERDE KESİNLİKLE DBTYPE GİRİLMEMELİ. DBTYPE OLANLAR SADECE OUTPUT PARAMETRELERİ<para />
-        /// TEKRAR TEKRAR AYNI DB NESNESI KULLANILACAKSA ARALARINDA FLUSHPARAMETRE() METODU KULLANILMALI <para />      
-        /// outputs=> gerıye donen output parametreleri<para />
-        /// Ek-Not: Float==>Single ama Float değerleri doublela karsılamak gerekiyor nedense.<para />
-        /// </summary>
-        public void SelectSP(string procadi, out List<Dparam> outputs)
-        {
-            try
-            {
-                var p = new DynamicParameters(); 
-                using (IDbConnection _db = new SqlConnection(Helper.connectionstring()))
-                {
-                    foreach (var item in parametreler) { p.Add(item.name, item.value); }
-                    foreach (var item in outputparametreler) { p.Add(item.name, dbType: item.dbtype, direction: ParameterDirection.Output); }
-                    _db.Query(procadi, p, commandType: System.Data.CommandType.StoredProcedure);
-                    outputs = new List<Dparam>();
-                    foreach (var item in outputparametreler) { outputs.Add(Dparam.typecaster(p, item.dbtype, item.name)); }
+					var donenliste = _db.Query<T>(procadi, p, commandType: System.Data.CommandType.StoredProcedure);
+					//var yorumlar = _db.Query<AracModel>("ap_AracModel", p, commandType: System.Data.CommandType.StoredProcedure);
+					List<T> sonliste = new List<T>();
+					foreach (var item in donenliste)
+						sonliste.Add(item);
 
-                }
-                
-            }
-            catch (Exception e)
-            {
-                e = e;
-                throw;
-            }
+					return sonliste;
+				}
+			}
+			catch (Exception e)
+			{
 
-        }
+				throw;
+			}
+			finally
+			{
+				if (flush)
+					FlushParameter();
+			}
+		}
+		/// <summary>***!!!OUTPUTLU PROCLAR ICIN.!!!***<para />
+		/// Dönüş değeri generictir. outputs nesnesi geriye name-value ikilisi olarak output degerleri doner.<para />
+		/// ÇOK ÖNEMLİ NOT: OUTPUT OLMAYAN PARAMETRELERDE KESİNLİKLE DBTYPE GİRİLMEMELİ. DBTYPE OLANLAR SADECE OUTPUT PARAMETRELERİ<para />
+		/// TEKRAR TEKRAR AYNI DB NESNESI KULLANILACAKSA ARALARINDA FLUSHPARAMETRE() METODU KULLANILMALI <para />
+		/// Generic metod oldugu için tiplerin baştan belirtilmesi gerekir. <para />
+		/// outputs=> gerıye donen output parametreleri<para />
+		/// Ek-Not: Float==>Single ama Float değerleri doublela karsılamak gerekiyor nedense.<para />
+		/// </summary>
+		public List<T> SelectSP<T>(string procadi, out List<Dparam> outputs,bool flush)
+		{
+			try
+			{
+				var p = new DynamicParameters(); List<T> donenliste = new List<T>();
+				using (IDbConnection _db = new SqlConnection(Helper.connectionstring()))
+				{
 
-        /// <summary>***Geri dönüş yapmayan ve outputu olmayan procedureler için***<para />
-        /// ÇOK ÖNEMLİ NOT: OUTPUT OLMAYAN PARAMETRELERDE KESİNLİKLE DBTYPE GİRİLMEMELİ. DBTYPE OLANLAR SADECE OUTPUT PARAMETRELERİ<para />
-        /// TEKRAR TEKRAR AYNI DB NESNESI KULLANILACAKSA ARALARINDA FLUSHPARAMETRE() METODU KULLANILMALI <para />      
-        /// outputs=> gerıye donen output parametreleri<para />
-        /// Ek-Not: Float==>Single ama Float değerleri doublela karsılamak gerekiyor nedense.<para />
-        /// </summary>
-        public void SelectSP(string procadi)
-        {
-            try
-            {
-                var p = new DynamicParameters();
-                using (IDbConnection _db = new SqlConnection(Helper.connectionstring()))
-                {
-                    foreach (var item in parametreler) { p.Add(item.name, item.value); }                 
-                    _db.Query(procadi, p, commandType: System.Data.CommandType.StoredProcedure);                 
-                }
+					foreach (var item in parametreler)
+						p.Add(item.name, item.value);
 
-            }
-            catch (Exception e)
-            {
-                e = e;
-                throw;
-            }
-
-        }
+					foreach (var item in outputparametreler)
+						p.Add(item.name, dbType: item.dbtype, direction: ParameterDirection.Output);
 
 
-        /// <summary>
-        /// Class based parametreler listesine sadece name-value ikilisi ile parametre ekler. Sadece gonderılen parametreler ıcın.Outputları kapsamaz. Aynı DB ile sorgularda FlushParameters kullanılmalı<para />
-        /// </summary>
-        public Dparam AddParameter(string name, object value)
-        {
-            try
-            {
-                Dparam param = new Dparam();
-                param.name = name; param.value = value;
-                this.parametreler.Add(param);
-                return param;//return cokta gereklı degıl aslında
-            }
-            catch (Exception e)
-            {
-                e = e;
-                throw;
-            }
-        }
-        /// <summary>
-        /// Class based parametreler listesine sadece name-value ikilisi ile parametre ekler. Sadece gonderılen parametreler ıcın.Outputları kapsamaz. Aynı DB ile sorgularda FlushParameters kullanılmalı<para />
-        /// </summary>
-        public Dparam AddOutputParameter(string name, DbType dbtype)
-        {
-            try
-            {               
-                Dparam param = new Dparam();
-                param.name = name; param.dbtype = dbtype;
-                this.outputparametreler.Add(param);
-                return param;
-            }
-            catch (Exception e)
-            {
-                e = e;
-                throw;
-            }
-        }
-        /// <summary>
-        /// İKİ İNSTANCE ARASINDA MUTLAKA KULLANILMALI. YOKSA PARAMETRELER KARISIR<para />
-        /// </summary>
-        public void FlushParameter()
-        {
-            parametreler = new List<Dparam>();
-            outputparametreler = new List<Dparam>();
-        }
+					var returner = _db.Query<T>(procadi, p, commandType: System.Data.CommandType.StoredProcedure);
+					foreach (var item in returner)
+						donenliste.Add(item);
+
+					outputs = new List<Dparam>();
+
+					foreach (var item in outputparametreler)
+						outputs.Add(Dparam.typecaster(p, item.dbtype, item.name));
+
+				}
+				//double a = p.Get<double>("@BORC");
+				//double b = p.Get<double>("@ALACAK");
+				return donenliste;
+			}
+			catch (Exception e)
+			{
+
+				throw;
+			}
+			finally
+			{
+				if (flush)
+					FlushParameter();
+			}
+
+		}
+
+
+		/// <summary>***Geri dönüş yapmayan procedureler için***<para />
+		/// ÇOK ÖNEMLİ NOT: OUTPUT OLMAYAN PARAMETRELERDE KESİNLİKLE DBTYPE GİRİLMEMELİ. DBTYPE OLANLAR SADECE OUTPUT PARAMETRELERİ<para />
+		/// TEKRAR TEKRAR AYNI DB NESNESI KULLANILACAKSA ARALARINDA FLUSHPARAMETRE() METODU KULLANILMALI <para />      
+		/// outputs=> gerıye donen output parametreleri<para />
+		/// Ek-Not: Float==>Single ama Float değerleri doublela karsılamak gerekiyor nedense.<para />
+		/// </summary>
+		public void SelectSP(string procadi, out List<Dparam> outputs,bool flush)
+		{
+			try
+			{
+				var p = new DynamicParameters();
+				using (IDbConnection _db = new SqlConnection(Helper.connectionstring()))
+				{
+					foreach (var item in parametreler)
+					{
+						p.Add(item.name, item.value);
+					}
+					foreach (var item in outputparametreler)
+					{
+						p.Add(item.name, dbType: item.dbtype, direction: ParameterDirection.Output);
+					}
+					_db.Query(procadi, p, commandType: System.Data.CommandType.StoredProcedure);
+					outputs = new List<Dparam>();
+					foreach (var item in outputparametreler) { outputs.Add(Dparam.typecaster(p, item.dbtype, item.name)); }
+
+				}
+
+			}
+			catch (Exception e)
+			{
+
+				throw;
+			}
+			finally
+			{
+				if (flush)
+					FlushParameter();
+			}
+
+		}
+
+		/// <summary>***Geri dönüş yapmayan ve outputu olmayan procedureler için***<para />
+		/// ÇOK ÖNEMLİ NOT: OUTPUT OLMAYAN PARAMETRELERDE KESİNLİKLE DBTYPE GİRİLMEMELİ. DBTYPE OLANLAR SADECE OUTPUT PARAMETRELERİ<para />
+		/// TEKRAR TEKRAR AYNI DB NESNESI KULLANILACAKSA ARALARINDA FLUSHPARAMETRE() METODU KULLANILMALI <para />      
+		/// outputs=> gerıye donen output parametreleri<para />
+		/// Ek-Not: Float==>Single ama Float değerleri doublela karsılamak gerekiyor nedense.<para />
+		/// </summary>
+		public void SelectSP(string procadi,bool flush)
+		{
+			try
+			{
+				var p = new DynamicParameters();
+				using (IDbConnection _db = new SqlConnection(Helper.connectionstring()))
+				{
+					foreach (var item in parametreler)
+						p.Add(item.name, item.value);
+
+					_db.Query(procadi, p, commandType: System.Data.CommandType.StoredProcedure);
+				}
+
+			}
+			catch (Exception e)
+			{
+
+				throw;
+			}
+			finally
+			{
+				if (flush)
+					FlushParameter();
+			}
+
+		}
+
+
+		/// <summary>
+		/// Class based parametreler listesine sadece name-value ikilisi ile parametre ekler. Sadece gonderılen parametreler ıcın.Outputları kapsamaz. Aynı DB ile sorgularda FlushParameters kullanılmalı<para />
+		/// </summary>
+		public Dparam AddParameter(string name, object value)
+		{
+			try
+			{
+				Dparam param = new Dparam();
+				param.name = name; param.value = value;
+				this.parametreler.Add(param);
+				return param;//return cokta gereklı degıl aslında
+			}
+			catch (Exception e)
+			{
+
+				throw;
+			}
+			
+		}
+		/// <summary>
+		/// Class based parametreler listesine sadece name-value ikilisi ile parametre ekler. Sadece gonderılen parametreler ıcın.Outputları kapsamaz. Aynı DB ile sorgularda FlushParameters kullanılmalı<para />
+		/// </summary>
+		public Dparam AddOutputParameter(string name, DbType dbtype)
+		{
+			try
+			{
+				Dparam param = new Dparam();
+				param.name = name; param.dbtype = dbtype;
+				this.outputparametreler.Add(param);
+				return param;
+			}
+			catch (Exception e)
+			{
+
+				throw;
+			}
+			
+		}
+		/// <summary>
+		/// İKİ İNSTANCE ARASINDA MUTLAKA KULLANILMALI. YOKSA PARAMETRELER KARISIR<para />
+		/// </summary>
+		public void FlushParameter()
+		{
+			parametreler = new List<Dparam>();
+			outputparametreler = new List<Dparam>();
+		}
 
 		public void Dispose()
-		{			
+		{
 			GC.SuppressFinalize(this);
 		}
 	}
--- DapperT2/Controllers/HomeController.cs
+++ DapperT2/Controllers/HomeController.cs
@@ -31,29 +31,45 @@ namespace DapperT2.Controllers
 					//4. parametreli output degerli donus degerli stored procedure
 
 					//****!!!******* 1
-					_db.SelectQuery<dynamic>("UPDATE AT_ANASAYFAMETINLER SET BASLIK = 'Lorem İpsum17', EKLEYENISIM = 'hbr' where ID=7");//No 1
+					_db.SelectQuery<dynamic>("UPDATE AT_ANASAYFAMETINLER SET BASLIK = 'Lorem İpsum17', EKLEYENISIM = 'hbr' where ID=7",false);//No 1
 					_db.FlushParameter();
 					//****!!!******* 2                        
 					_db.AddParameter("@ID", 221831);
 					_db.AddParameter("@ADET", 0);
 					_db.AddParameter("@NOT", "hb");
-					_db.SelectSP("ap_Sepet_Guncelle_Web");//2. numaralı
+					_db.SelectSP("ap_Sepet_Guncelle_Web",false);//2. numaralı
 					_db.FlushParameter();
 					//****!!!*******3
 					_db.AddParameter("@MARKA", "ALFA ROMEO");
-					var list = _db.SelectSP<AracModel>("ap_AracModel");
+					var list = _db.SelectSP<AracModel>("ap_AracModel",false);
 					_db.FlushParameter();
 					//****!!!*******4
 					List<DDLayer.Access.Dparam> outputs = new List<DDLayer.Access.Dparam>();
 					_db.AddParameter("@CARIKOD", "M000");
 					_db.AddOutputParameter("@BORC", DbType.Double);
 					_db.AddOutputParameter("@ALACAK", DbType.Double);
-					var list2 = _db.SelectSP<CARILISTE>("ap_GetCariHareket", out outputs);
+					var list2 = _db.SelectSP<CARILISTE>("ap_GetCariHareket", out outputs,false);
 					//outputs dönüş değerleridir
+
+					//otomatik flush parameter olanlar										
+					_db.SelectQuery<dynamic>("UPDATE AT_ANASAYFAMETINLER SET BASLIK = 'Lorem İpsum17', EKLEYENISIM = 'hbr' where ID=7", true); 
+					_db.AddParameter("@ID", 221831);
+					_db.AddParameter("@ADET", 0);
+					_db.AddParameter("@NOT", "hb");
+					_db.SelectSP("ap_Sepet_Guncelle_Web", true);//2. numaralı										
+					_db.AddParameter("@MARKA", "ALFA ROMEO");
+					var list3 = _db.SelectSP<AracModel>("ap_AracModel", true);										
+					List<DDLayer.Access.Dparam> outputs2 = new List<DDLayer.Access.Dparam>();
+					_db.AddParameter("@CARIKOD", "M000");
+					_db.AddOutputParameter("@BORC", DbType.Double);
+					_db.AddOutputParameter("@ALACAK", DbType.Double);
+					var list4 = _db.SelectSP<CARILISTE>("ap_GetCariHareket", out outputs, true);
+					//outputs2 dönüş değerleridir
+
 				}
 				catch (Exception E)
 				{
-					E = E;
+					
 					throw;
 				}
 			}
